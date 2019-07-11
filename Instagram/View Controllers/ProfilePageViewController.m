07898be6e9c00a04958d2d0ee20318ce5a18a499
Ilana Shapiro <ilanashapiro@fb.com>
2019-07-11 12:48:41 -0700

    added empty profile view accessed modally thru username button on feed

---
+++ Instagram/View Controllers/ProfilePageViewController.m
@@ -0,0 +1,32 @@
+//
+//  ProfilePageViewController.m
+//  Instagram
+//
+//  Created by ilanashapiro on 7/11/19.
+//  Copyright © 2019 ilanashapiro. All rights reserved.
+//
+
+#import "ProfilePageViewController.h"
+
+@interface ProfilePageViewController ()
+
+@end
+
+@implementation ProfilePageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
